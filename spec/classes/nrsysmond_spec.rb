bf66a5c4cbee245424811b2ab93562773095a6eb
Jesse Dearing <jesse.dearing@gmail.com>
2012-11-08 14:06:01 -0800

    Adds tests for RedHat

--- spec/classes/nrsysmond_spec.rb
+++ spec/classes/nrsysmond_spec.rb
@@ -3,9 +3,22 @@ require 'spec_helper'
 describe 'nrsysmond' do
   let(:params) { {:license_key => '14758f1afd44c09b7992073ccf00b43d' }}
 
-  context 'RedHat osfamily' do
-    let(:facts) { {:osfamily => 'RedHat'} }
-    it { should include_class('nrsysmond::params')}
+  # ['RedHat', 'Ubuntu'].each do |platform|
+  ['RedHat'].each do |platform|
+    context '#{platform} osfamily' do
+      let(:facts) { {:osfamily => platform} }
+      it { should include_class('nrsysmond::params')}
+
+      it { should contain_package 'newrelic-sysmond'}
+
+      it { should contain_class('nrsysmond::config').with(
+        'license_key' => '14758f1afd44c09b7992073ccf00b43d',
+        'nrlogfile'   => '/var/log/newrelic/nrsysmond.log',
+        'nrloglevel'  => 'info'
+      )}
+
+      it { should contain_service 'newrelic-sysmond' }
+    end
   end
 
   context 'Non-Ubuntu and non-RedHat osfamily' do
@@ -15,4 +28,5 @@ describe 'nrsysmond' do
       }.to raise_error(Puppet::Error, /not supported/)
     end
   end
+
 end
