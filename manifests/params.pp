55dfe1f101f7a2df00bd911415820f13cc07127b
Jacob Bednarz <jacob.bednarz@gmail.com>
2015-10-08 17:42:08 +1100

    Use HTTPS everywhere for New Relic endpoints
    New Relic support HTTPS on these endpoints so let's use it!

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,13 +6,13 @@ class nrsysmond::params {
   case $::osfamily {
     'RedHat': {
       if $::hardwaremodel == 'x86_64' {
-        $rpm_repo_location = 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
+        $rpm_repo_location = 'https://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
       } else {
-        $rpm_repo_location = 'http://yum.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
+        $rpm_repo_location = 'https://yum.newrelic.com/pub/newrelic/el5/i386/newrelic-repo-5-3.noarch.rpm'
       }
     }
     'Debian': {
-      $apt_repo = 'http://apt.newrelic.com/debian/'
+      $apt_repo = 'https://apt.newrelic.com/debian/'
     }
     default: {
       fail("ERROR: nrsysmond is not supported on osfamily ${::osfamily}")
